deb76d803d07c6af37c091c851dc74ac024794f8
u-keisuke <u.keisuke1111@gmail.com>
2024-01-09 16:38:07 +0900

    Fix select_valid_function func to assure gradient tracking. Replace np.random.choice by torch.multinomial.

--- l2e/src/utils_l2e.py
+++ l2e/src/utils_l2e.py
@@ -1,10 +1,20 @@
 import numpy as np
+import torch
 import torch.nn.functional as F
 
 
-def select_valid_action(out, valid_actions_str, action_space):
+def select_valid_action(out, valid_actions_str: list, action_space: list):
     valid_actions_idx = [action_space.index(a) for a in valid_actions_str]
+
     out_valid = out[:, valid_actions_idx]
-    actions_prob = F.softmax(out_valid, dim=1).detach().numpy()
-    selected_action = np.random.choice(valid_actions_idx, p=actions_prob.reshape(-1))
+    actions_prob = F.softmax(out_valid, dim=1)
+
+    # selected_action = np.random.choice(valid_actions_idx, p=actions_prob.reshape(-1))
+
+    # torch.multinomialを使って、確率に基づいてアクションをサンプリング
+    selected_action_idx = torch.multinomial(actions_prob, num_samples=1)
+
+    # 選択されたアクションのインデックスを取得し、元のアクションスペースにマッピング
+    selected_action = valid_actions_idx[selected_action_idx.item()]
+
     return selected_action
